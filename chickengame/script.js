137b59434c8096b90439cd433b2fbd08591cf95c
Vincent Wong <vincentwong2715@gmail.com>
2023-01-28 20:55:14 -0600

    Bugfixes + Reformatting

--- chickengame/script.js
+++ chickengame/script.js
@@ -7,6 +7,7 @@ const gridHeight = 6;
 const grid = [];
 let frame = 1;
 let score = 0;
+let ingame = false;
 var soundtrack;
 
 //adjustable by user
@@ -20,8 +21,8 @@ chicken.src = 'assets/chicken.jpg';
 let warning = new Image();
 warning.src = 'assets/danger.png';
 
-class GridSquare{
-  constructor(a, b){
+class GridSquare {
+  constructor(a, b) {
     this.x = a * 100;
     this.y = b * 100;
     this.width = 100;
@@ -31,21 +32,21 @@ class GridSquare{
     this.active = false;
     this.activationFrame = -1;
   }
-  draw(){
-    if (this.active){
+  draw() {
+    if (this.active) {
       //ctx.fillStyle = "red";
       //ctx.fillRect(this.x, this.y, this.width, this.height);
       ctx.drawImage(chicken, this.x, this.y, this.width, this.height);
     }
-    else if(this.warning){
+    else if (this.warning) {
       //ctx.fillStyle = "orange";
       //ctx.fillRect(this.x, this.y, this.width, this.height);
       ctx.drawImage(warning, this.x, this.y, this.width, this.height);
     }
     ctx.strokeRect(this.x, this.y, this.width, this.height);
   }
-  loudsquawk(){
-    let num = Math.ceil(Math.random()*6);
+  loudsquawk() {
+    let num = Math.ceil(Math.random() * 6);
     var squawk = new Audio(`assets/squawk${num}.wav`);
     console.log("SQUAWK");
     squawk.play();
@@ -53,67 +54,70 @@ class GridSquare{
 }
 
 //Populate Grid Array
-for(let y = 0 ; y< gridHeight; y++){
-  for (let x = 0; x < gridWidth; x++){
-    grid.push(new GridSquare(x,y));
+function populate() {
+  for (let y = 0; y < gridHeight; y++) {
+    for (let x = 0; x < gridWidth; x++) {
+      grid.push(new GridSquare(x, y));
+    }
   }
 }
 
-function randomize(size, times){
+function randomize(size, times) {
   for (let i = 0; i < times; i++) {
     let chicken = Math.floor(Math.random() * size);
     grid[chicken].warning = true;
   }
 }
 
-function activate(){
-  grid.forEach((square)=>{
-    if(square.warning){
-      if(frame - square.warningFrame >= timeLimit)
-      square.warning = false;
-      square.active = true;
-      square.activationFrame = frame;
-      square.loudsquawk();
-  }
+function activate() {
+  grid.forEach((square) => {
+    if (square.warning) {
+      if (frame - square.warningFrame >= timeLimit) {
+        square.warning = false;
+        square.active = true;
+        square.activationFrame = frame;
+        square.loudsquawk();
+      }
+    }
   })
 }
 
-function deactivate(){
-  grid.forEach((square)=>{
-    if(square.active){
-      if(frame - square.activationFrame >= timeLimit){
+function deactivate() {
+  grid.forEach((square) => {
+    if (square.active) {
+      if (frame - square.activationFrame >= timeLimit) {
         square.active = false;
         hearts--;
       }
-  }
+    }
   })
 }
 
-function determineLocation(x,y){
-  x = Math.floor(x/100);
-  y = Math.floor(y/100);
+function determineLocation(x, y) {
+  x = Math.floor(x / 100);
+  y = Math.floor(y / 100);
   let result = 0;
-  if(x>CANVAS_WIDTH || y>CANVAS_HEIGHT)
+  if (x > CANVAS_WIDTH || y > CANVAS_HEIGHT)
     return -1;
-  while(true){
-    if(x == 0 && y == 0)
+  while (true) {
+    if (x == 0 && y == 0)
       return result;
-    else if(y>0){
+    else if (y > 0) {
       result += 9;
       y--;
     }
-    else if(x>0){
+    else if (x > 0) {
       result++;
       x--;
     }
   }
 }
 
-function swingHammer(x, y){
-  let squareHit = determineLocation(x,y);
-  
-  if(squareHit > -1){
-    if(grid[squareHit].active){
+function swingHammer(x, y) {
+  let squareHit = determineLocation(x, y);
+
+  if (squareHit > -1) {
+    if (grid[squareHit].active) {
       grid[squareHit].active = false;
       score++;
       document.getElementById('score').innerHTML = `Score: ${score}`;
@@ -130,59 +134,62 @@ function getMousePosition(canvas, event) {
 }
 
 let canvasElem = document.getElementById("canvas1");
-canvasElem.addEventListener("mousedown", function(e)
-{
+canvasElem.addEventListener("mousedown", function (e) {
   getMousePosition(canvasElem, e);
 });
 
-function start(){
-  let form = document.getElementById("cxnfig");
-  spawnCount = form.elements[0].value;
-  timeLimit = form.elements[1].value;
-  hearts = form.elements[2].value;
-  animate();
+function start() {
+  if (!ingame) {
+    ingame = true;
+    let form = document.getElementById("cxnfig");
+    spawnCount = form.elements[0].value;
+    timeLimit = form.elements[1].value;
+    hearts = form.elements[2].value;
+    populate();
+    animate();
 
-  //Play Soundtrack
-  soundtrack = new Audio('assets/squawk-idle.wav');
-  soundtrack.loop = true;
-  soundtrack.play();
+    //Play Soundtrack
+    soundtrack = new Audio('assets/squawk-idle.wav');
+    soundtrack.loop = true;
+    soundtrack.play();
+  }
 }
 
-function gameOver(){
+function gameOver() {
   //soundtrack.pause();
 
   ctx.fillStyle = "black";
-  ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   ctx.fillStyle = "rgba(255,0,0,1)";
   ctx.font = "bold 30px FranklinGothicMedium";
-  
+
   ctx.textAlign = "center";
-  ctx.fillText("GAME OVER", CANVAS_WIDTH/2, 100);
-  ctx.fillText(`Your score was: ${score}`, CANVAS_WIDTH/2, 150);
-  ctx.fillText(`First time headphone users, did you enjoy yourselves?`, CANVAS_WIDTH/2, 350);
+  ctx.fillText("GAME OVER", CANVAS_WIDTH / 2, 100);
+  ctx.fillText(`Your score was: ${score}`, CANVAS_WIDTH / 2, 150);
+  ctx.fillText(`First time headphone users, did you enjoy yourselves?`, CANVAS_WIDTH / 2, 350);
 }
 
 //Main Animate Function
-function animate(){
-  ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-  if(frame % timeLimit == 0){
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  if (frame % timeLimit == 0) {
     randomize(gridHeight * gridWidth, spawnCount);
   }
-  if(frame % timeLimit == (timeLimit/2)){
+  if (frame % timeLimit == Math.floor((timeLimit / 2))) {
     activate();
     deactivate();
     document.getElementById('heartcount').innerHTML = `Heart Count: ${hearts}`;
   }
 
-  grid.forEach(square =>{
+  grid.forEach(square => {
     square.draw();
   })
-  
+
   frame++;
-  if(hearts > 0){
+  if (hearts > 0) {
     window.requestAnimationFrame(animate);
   }
-  else{
+  else {
     gameOver();
   }
 }
